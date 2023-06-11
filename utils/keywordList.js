bf8c0a41b2d61f5642c806a82410ed1fb83b0f08
cabrera-evil <douglascabrera.dev@gmail.com>
2023-06-11 15:48:13 -0600

    Update: Keyword filer

--- utils/keywordList.js
+++ utils/keywordList.js
@@ -1,120 +1,27 @@
-const disorders = [
-    'abuso de drogas',
-    'abuso de sustancias',
-    'codependencia',
-    'depresión',
-    'desorden alimenticio',
-    'desorden emocional',
-    'duelo',
-    'inestabilidad emocional',
-    'insomnio',
-    'irritabilidad',
-    'maltrato emocional',
-    'miedo',
-    'psiquiatría',
-    'psitacosis',
-    'trastorno',
-    'alimentario',
-    'ansiedad',
-    'estrés postraumático',
-    'personalidad',
-    'transtorno emocional',
-    'obsesivo compulsivo',
-    'psicológico',
-    'psiquiátrico',
-    'alimentarios',
-    'estado de ánimo',
-    'sueño',
-    'mentales'
-];
+const disorders = require('./keywords/disorders');
+const problems = require('./keywords/problems');
+const selfHelp = require('./keywords/selfHelp');
+const therapy = require('./keywords/therapy');
+const greetings = require('./keywords/greetings');
+const psychologistTopics = require('./keywords/psychologistTopics');
+const hobbies = require('./keywords/hobbies');
+const relationships = require('./keywords/relationships');
+const personalDevelopment = require('./keywords/personalDevelopment');
+const workLife = require('./keywords/workLife');
+const otherTopics = require('./keywords/otherTopics');
 
-const problems = [
-    'problemas',
-    'aflicción',
-    'ansia',
-    'ansiedad',
-    'cansancio',
-    'desánimo',
-    'desasosiego',
-    'desesperación',
-    'desgaste emocional',
-    'desmotivación',
-    'emociones negativas',
-    'frustración',
-    'herida emocional',
-    'heridas del alma',
-    'lucha emocional',
-    'preocupación',
-    'emocionales',
-    'familiares',
-    'laborales',
-    'trauma',
-    'tristeza',
-    'vulnerabilidad emocional'
+const keywords = [
+    ...disorders,
+    ...problems,
+    ...selfHelp,
+    ...therapy,
+    ...greetings,
+    ...psychologistTopics,
+    ...hobbies,
+    ...relationships,
+    ...personalDevelopment,
+    ...workLife,
+    ...otherTopics
 ];
-
-const selfHelp = [
-    'acompañamiento',
-    'amor propio',
-    'autoayuda',
-    'autoconocimiento',
-    'autocuidado',
-    'autoestima',
-    'autoexigencia',
-    'autoexploración',
-    'bienestar',
-    'bienestar emocional',
-    'capacidad de adaptación',
-    'comunicación efectiva',
-    'confianza en uno mismo',
-    'consejería',
-    'consuelo',
-    'cuidado personal',
-    'desarrollo personal',
-    'empatía',
-    'energía emocional',
-    'entendimiento interpersonal',
-    'entrenamiento emocional',
-    'equilibrio emocional',
-    'esfuerzo personal',
-    'familia',
-    'fatiga',
-    'felicidad',
-    'fortalecimiento emocional',
-    'habilidades sociales',
-    'meditación',
-    'optimismo',
-    'orientación psicológica',
-    'paciencia',
-    'pareja',
-    'paz mental',
-    'perspectiva emocional',
-    'problemas de la vida cotidiana',
-    'psicología',
-    'recuperación emocional',
-    'reducción del estrés',
-    'rehabilitación emocional',
-    'relación',
-    'relajación',
-    'resiliencia',
-    'salud emocional',
-    'salud mental',
-    'sanación emocional',
-    'satisfacción personal',
-    'sexualidad',
-    'autoconocimiento',
-    'autocuidado',
-    'autoestima',
-    'autoexigencia',
-    'autoexploración'
-];
-
-const therapy = [
-    'cognitivo conductual',
-    'consejería',
-    'psicológica',
-];
-
-const keywords = disorders.concat(problems, selfHelp, therapy);
 
 module.exports = keywords;
