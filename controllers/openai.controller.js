98ae4d638d044e69c9ae38a5783c95bc7f697f94
cabrera-evil <douglascabrera.dev@gmail.com>
2023-06-24 21:01:20 -0600

    Add: save messages into firebase as a conversation

--- controllers/openai.controller.js
+++ controllers/openai.controller.js
@@ -1,4 +1,6 @@
 const { config } = require("../config/gpt.config");
+const { auth, db } = require('../config/firebase.config');
+const { collection, doc, setDoc, getDoc, getDocs } = require('firebase/firestore');
 
 /**
  * Retrieves all the available OpenAI engines
@@ -20,17 +22,33 @@ const getEngines = async (req, res) => {
  * @param {Object} req - The HTTP request object
  * @param {Object} res - The HTTP response object
  */
-
 const postChatRequest = async (req, res) => {
     const openai = await config();
-    const { model, messages } = req.body;
+    const { messages, userId } = req.body;
+
+    // Find chat in firebase
+    const chatRef = collection(db, 'userChats');
+    const querySnapshot = await getDocs(chatRef);
+
+    const chats = [];
+    querySnapshot.forEach((doc) => {
+        chats.push({ id: doc.id, ...doc.data() });
+    });
+
+    // Hacer que coincida el nombre del documento con el id del usuario
+    const chat = chats.find((chat) => chat.id === userId);
 
     // Create an array to hold the chat messages
-    const chatMessages = [];
+    let chatMessages = [];
+
+    // Retrieve existing messages from the chat or initialize an empty array
+    if (chat && chat.messages) {
+        chatMessages = chat.messages;
+    }
 
     // Loop through each message in the request body
     for (const message of messages) {
-        // Get the content of the message
+        // Get the content and role of the message
         const chatContent = message.content;
         const chatRole = message.role;
 
@@ -47,10 +65,26 @@ const postChatRequest = async (req, res) => {
     // Send the chat request to OpenAI
     const openaiResponse = await openai.createChatCompletion(chatRequest);
     const chatResponse = openaiResponse.data.choices[0].message;
+    const chatRole = openaiResponse.data.choices[0].role;
+
+    // Add the chat response to the chat messages array
+    chatMessages.push({ role: "assistant", content: chatResponse.content });
+
+    // Update the chat in Firebase
+    if (chat) {
+        await setDoc(doc(db, 'userChats', userId), {
+            messages: chatMessages,
+        });
+    } else {
+        await setDoc(doc(db, 'userChats', userId), {
+            messages: chatMessages,
+        });
+    }
 
     // Return the chat response to the client
     res.status(200).json({
-        chatResponse: chatResponse,
+        message: chatResponse,
+        role: chatRole
     });
 };
 
